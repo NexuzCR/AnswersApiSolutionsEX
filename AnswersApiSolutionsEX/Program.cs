49424a1814b6d23793325f30c797087e51cad6c8
NexuzCR <isavalverde543@gmail.com>
2023-08-23 16:59:19 -0600

    Add project files.

---
+++ AnswersApiSolutionsEX/Program.cs
@@ -0,0 +1,44 @@
+using AnswersApiSolutionsEX.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace AnswersApiSolutionsEX
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            var CnnStrBuilder = new SqlConnectionStringBuilder(builder.Configuration.GetConnectionString("CNNSTR"));
+            CnnStrBuilder.Password = "123456";
+            string cnnStr = CnnStrBuilder.ConnectionString;
+            builder.Services.AddDbContext<AnswersDBContext>(options => options.UseSqlServer(cnnStr));
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
